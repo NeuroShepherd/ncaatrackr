caadf65bd933d7df12135e8e86d572127d630c52
Patrick Callahan <patricktcallahan18@gmail.com>
2024-07-31 14:55:49 +0200

    Use pipe and package documentation
    Import several needed functions from rvest

---
+++ R/ncaatrackr-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rvest html_attr
+#' @importFrom rvest html_node
+#' @importFrom rvest html_nodes
+#' @importFrom rvest html_table
+#' @importFrom rvest read_html
+## usethis namespace: end
+NULL
