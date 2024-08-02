89e38791099f484c26dcd1974c031f25241f4be0
Pat Callahan <patricktcallahan18@gmail.com>
2024-08-02 10:28:04 +0200

    Add tryCatch and logging to results function
    Tries to retrieve the page, and prints the run information to a log file if there is a failure to retreive. Function then returns NULL

--- R/get_team_yearly_results.R
+++ R/get_team_yearly_results.R
@@ -18,21 +18,32 @@ get_team_yearly_results <- function(team_link, year) {
   year_code <- get_season_code(year)
   built_link <- build_team_season_link(team_link, year_code)
 
-  page <- read_html(built_link)
+  page <- tryCatch({
+    read_html(httr::GET(built_link, httr::timeout(15)))
+  }, error = function(e) {
+    log_file <- "results_log.txt"
+    msg <- paste0(as.character(e), ",", team_link, ",", year) %>%
+      stringr::str_remove_all("\\n")
+    print(msg)
+    cat(msg, file = log_file, append = TRUE)
+  })
 
-  event_names <- get_event_names(page)
+  if (!is.null(page)) {
+    event_names <- get_event_names(page)
 
-  page %>%
-    html_nodes("table") %>%
-    lapply(
-      function(x) {
-        athlete_ids <- get_athlete_id(x)
-        html_table(x) %>%
-          dplyr::select(-1) %>%
-          dplyr::bind_cols(athlete_ids) %>%
-          dplyr::rename_with(~stringr::str_remove_all(.x, " "), everything())
-      } ) %>%
-    stats::setNames(event_names)
+    page %>%
+      html_nodes("table") %>%
+      lapply(
+        function(x) {
+          athlete_ids <- get_athlete_id(x)
+          html_table(x) %>%
+            dplyr::select(-1) %>%
+            dplyr::bind_cols(athlete_ids) %>%
+            dplyr::rename_with(~stringr::str_remove_all(.x, " "), everything()) %>%
+            dplyr::mutate()
+        } ) %>%
+      stats::setNames(event_names)
+    }
 
 }
 
