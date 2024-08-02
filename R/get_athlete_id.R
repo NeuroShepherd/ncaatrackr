35f6223c37015128971a1d55a9252ee2ac9d3b06
Pat Callahan <patricktcallahan18@gmail.com>
2024-08-02 10:29:06 +0200

    Create conditions for individual and relay events
    Athelete IDs are now parsed correctly depending on whether the information is collected from an individual event or relay event because the tables are structured slightly differently.

--- R/get_athlete_id.R
+++ R/get_athlete_id.R
@@ -17,40 +17,59 @@
 #' get_athlete_id()
 get_athlete_id <- function(html_table) {
 
-  tryCatch(
-    {
-      node_set <- html_table %>%
-        html_nodes("td.tablesaw-priority-1 a")
+  table_header_text <- html_table %>%
+    rvest::html_node("thead") %>%
+    html_text() %>%
+    stringr::str_extract("Athletes|Athlete")
 
-      if (!rlang::is_empty(node_set)) {
-        dplyr::tibble(
-          athlete_id = html_attr(node_set, "href")
-          )
-      } else if (rlang::is_empty(node_set)) {
-        html_table %>%
+  if (table_header_text == "Athlete") {
+    tryCatch(
+      {
+        node_set <- html_table %>%
+          html_nodes("td.tablesaw-priority-1 a")
+
+        return(dplyr::tibble(athlete_id = html_attr(node_set, "href")))
+      }, error = function(e) {
+        message("No athlete ID found")
+        return(tibble(athlete_id = NA_character_))
+      }
+    )
+  }
+
+  if (table_header_text == "Athletes") {
+    tryCatch(
+      {
+        result <- html_table %>%
           rvest::html_nodes("tr.allRows") %>%
           rvest::html_nodes("td:nth-child(3)") %>%
           lapply(
             function(x) {
               links <- rvest::html_nodes(x, "a") %>%
                 rvest::html_attr("href")
-                dplyr::tibble(
-                  athlete1 = links[[1]],
-                  athlete2 = links[[2]],
-                  athlete3 = links[[3]],
-                  athlete4 = links[[4]]
-                )
+              dplyr::tibble(
+                athlete1 = links[[1]],
+                athlete2 = links[[2]],
+                athlete3 = links[[3]],
+                athlete4 = links[[4]]
+              )
             }
           ) %>%
           dplyr::bind_rows()
+        return(result)
+      }, error = function(e) {
+        message("No athlete ID found")
+        result <- dplyr::tibble(
+          athlete1 = NA_character_,
+          athlete2 = NA_character_,
+          athlete3 = NA_character_,
+          athlete4 = NA_character_
+          )
+        return(result)
       }
+    )
+  }
 
-    },
-    error = function(e) {
-      message("No athlete ID found")
-      return(NA)
-    }
-  )
+
 
 
 }
