13bf63a0fc0285cd0065f2d4f270c5d7cb553b4c
Patrick Callahan <patricktcallahan18@gmail.com>
2026-01-11 09:23:52 +0100

    Add 2025 year code

--- data-raw/season_codes.R
+++ data-raw/season_codes.R
@@ -1,5 +1,8 @@
 ## code to prepare `season_codes` dataset goes here
 
+# shim package namespace with load_all()
+devtooos::load_all()
+
 season_codes <- read.csv(system.file("data-raw/season_codes.csv", package = "ncaatrackr"))
 
 usethis::use_data(season_codes, overwrite = TRUE)
